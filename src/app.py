fde833e8db5f71295bf0fb0bc65dcf72920ac2c2
Emil Lozev <em.lozev@gmail.com>
2024-11-10 10:07:02 +0000

    style: fmt

--- src/app.py
+++ src/app.py
@@ -1,29 +1,33 @@
 from flask import Flask, request, jsonify
-
 from src.classifier import classify_file
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
 
-ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg'}
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+  """Check if the file extension is allowed."""
+  return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/classify_file', methods=['POST'])
+@app.route('/classify-file', methods=['POST'])
 def classify_file_route():
+  """Classify the file and return the file class."""
+  if 'file' not in request.files:
+    return jsonify({"error": "No file part in the request"}), 400
 
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part in the request"}), 400
+  file = request.files['file']
+  if file.filename == '':
+    return jsonify({"error": "No selected file"}), 400
 
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({"error": "No selected file"}), 400
+  if not allowed_file(file.filename):
+    return jsonify({"error": f"File type {file.filename.split('.')[1].lower()} not allowed"}), 400
 
-    if not allowed_file(file.filename):
-        return jsonify({"error": f"File type not allowed"}), 400
-
-    file_class = classify_file(file)
-    return jsonify({"file_class": file_class}), 200
+  file_class = classify_file(file)
+  return jsonify({"file_class": file_class}), 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
--- src/classifier.py
+++ src/classifier.py
@@ -1,17 +1,16 @@
 from werkzeug.datastructures import FileStorage
 
 def classify_file(file: FileStorage):
-    filename = file.filename.lower()
-    # file_bytes = file.read()
+  filename = file.filename.lower()
+  # file_bytes = file.read()
 
-    if "drivers_license" in filename:
-        return "drivers_licence"
+  if "drivers_license" in filename:
+    return "drivers_licence"
 
-    if "bank_statement" in filename:
-        return "bank_statement"
+  if "bank_statement" in filename:
+    return "bank_statement"
 
-    if "invoice" in filename:
-        return "invoice"
+  if "invoice" in filename:
+    return "invoice"
 
-    return "unknown file"
-
+  return "unknown file"
